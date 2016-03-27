37cb1f4d312c9697da1734cb6bbf6b6bbe213a69
Jonathan Carter <borkabrak@gmail.com>
2016-03-27 17:07:43 -0500

    Formatting, mostly.

--- attachEvents.js
+++ attachEvents.js
@@ -36,12 +36,11 @@ var attachEvents = function(behaviors, element) {
 
     var keymap = {};
 
-    Object.keys(behaviors).forEach( function(target) {
-
+    Object.keys(behaviors).forEach(function(target) {
         // target is a selector.  Attach the actions to matching elements.
-        if ( document.querySelector(target) ) {
-            Array.prototype.slice.call(document.querySelectorAll(target)).forEach(function(element) {
-                attachEvents(behaviors[target], element);
+        if (document.querySelector(target)) {
+            Array.prototype.slice.call(document.querySelectorAll(target)).forEach(function(element) { 
+                attachEvents(behaviors[target], element); 
             });
 
         // single characters specify keypresses.  Add them to the keymap, which
@@ -54,7 +53,7 @@ var attachEvents = function(behaviors, element) {
             element.addEventListener(target, behaviors[target])
 
         }
-    });
+    } );
 
     // Handle keypresses.
     element.addEventListener("keypress", function(event) {
@@ -66,42 +65,41 @@ var attachEvents = function(behaviors, element) {
 
 // Reference list of all events in the official standard?
 var standard_events = [
-    "abort", "abort", "abort", "afterprint", "animationend",
-    "animationiteration", "animationstart", "audioprocess", "beforeprint",
-    "beforeunload", "beginEvent", "blocked", "blur", "cached", "canplay",
-    "canplaythrough", "change", "chargingchange", "chargingtimechange",
-    "checking", "click", "close", "complete", "complete", "compositionend",
-    "compositionstart", "compositionupdate", "contextmenu", "copy", "cut",
-    "dblclick", "devicelight", "devicemotion", "deviceorientation",
-    "deviceproximity", "dischargingtimechange", "DOMActivate",
-    "DOMAttributeNameChanged", "DOMAttrModified", "DOMCharacterDataModified",
-    "DOMContentLoaded", "DOMElementNameChanged", "DOMNodeInserted",
-    "DOMNodeInsertedIntoDocument", "DOMNodeRemoved",
-    "DOMNodeRemovedFromDocument", "DOMSubtreeModified", "downloading", "drag",
-    "dragend", "dragenter", "dragleave", "dragover", "dragstart", "drop",
-    "durationchange", "emptied", "ended", "ended", "endEvent", "error",
-    "error", "error", "error", "error", "error", "focus", "fullscreenchange",
-    "fullscreenerror", "gamepadconnected", "gamepaddisconnected",
-    "gotpointercapture", "hashchange", "lostpointercapture", "input",
-    "invalid", "keydown", "keypress", "keyup", "languagechange", "levelchange",
-    "load", "load", "loadeddata", "loadedmetadata", "loadend", "loadstart",
-    "message", "message", "message", "message", "mousedown", "mouseenter",
-    "mouseleave", "mousemove", "mouseout", "mouseover", "mouseup",
-    "notificationclick", "noupdate", "obsolete", "offline", "online", "open",
-    "open", "orientationchange", "pagehide", "pageshow", "paste", "pause",
-    "pointercancel", "pointerdown", "pointerenter", "pointerleave",
-    "pointerlockchange", "pointerlockerror", "pointermove", "pointerout",
-    "pointerover", "pointerup", "play", "playing", "popstate", "progress",
-    "progress", "push", "pushsubscriptionchange", "ratechange",
-    "readystatechange", "repeatEvent", "reset", "resize", "scroll", "seeked",
-    "seeking", "select", "selectstart", "selectionchange", "show", "stalled",
-    "storage", "submit", "success", "suspend", "SVGAbort", "SVGError",
-    "SVGLoad", "SVGResize", "SVGScroll", "SVGUnload", "SVGZoom", "timeout",
-    "timeupdate", "touchcancel", "touchend", "touchenter", "touchleave",
-    "touchmove", "touchstart", "transitionend", "unload", "updateready",
-    "upgradeneeded", "userproximity", "versionchange", "visibilitychange",
-    "volumechange", "waiting", "wheel"
- ];
+    "abort", "afterprint", "animationend", "animationiteration",
+    "animationstart", "audioprocess", "beforeprint", "beforeunload",
+    "beginEvent", "blocked", "blur", "cached", "canplay", "canplaythrough",
+    "change", "chargingchange", "chargingtimechange", "checking", "click",
+    "close", "complete", "complete", "compositionend", "compositionstart",
+    "compositionupdate", "contextmenu", "copy", "cut", "dblclick",
+    "devicelight", "devicemotion", "deviceorientation", "deviceproximity",
+    "dischargingtimechange", "DOMActivate", "DOMAttributeNameChanged",
+    "DOMAttrModified", "DOMCharacterDataModified", "DOMContentLoaded",
+    "DOMElementNameChanged", "DOMNodeInserted", "DOMNodeInsertedIntoDocument",
+    "DOMNodeRemoved", "DOMNodeRemovedFromDocument", "DOMSubtreeModified",
+    "downloading", "drag", "dragend", "dragenter", "dragleave", "dragover",
+    "dragstart", "drop", "durationchange", "emptied", "ended", "ended",
+    "endEvent", "error", "error", "error", "error", "error", "error", "focus",
+    "fullscreenchange", "fullscreenerror", "gamepadconnected",
+    "gamepaddisconnected", "gotpointercapture", "hashchange",
+    "lostpointercapture", "input", "invalid", "keydown", "keypress", "keyup",
+    "languagechange", "levelchange", "load", "load", "loadeddata",
+    "loadedmetadata", "loadend", "loadstart", "message", "message", "message",
+    "message", "mousedown", "mouseenter", "mouseleave", "mousemove",
+    "mouseout", "mouseover", "mouseup", "notificationclick", "noupdate",
+    "obsolete", "offline", "online", "open", "open", "orientationchange",
+    "pagehide", "pageshow", "paste", "pause", "pointercancel", "pointerdown",
+    "pointerenter", "pointerleave", "pointerlockchange", "pointerlockerror",
+    "pointermove", "pointerout", "pointerover", "pointerup", "play", "playing",
+    "popstate", "progress", "progress", "push", "pushsubscriptionchange",
+    "ratechange", "readystatechange", "repeatEvent", "reset", "resize",
+    "scroll", "seeked", "seeking", "select", "selectstart", "selectionchange",
+    "show", "stalled", "storage", "submit", "success", "suspend", "SVGAbort",
+    "SVGError", "SVGLoad", "SVGResize", "SVGScroll", "SVGUnload", "SVGZoom",
+    "timeout", "timeupdate", "touchcancel", "touchend", "touchenter",
+    "touchleave", "touchmove", "touchstart", "transitionend", "unload",
+    "updateready", "upgradeneeded", "userproximity", "versionchange",
+    "visibilitychange", "volumechange", "waiting", "wheel" 
+];
 
  // hoist this function to global scope.
  window.attachEvents = attachEvents;
